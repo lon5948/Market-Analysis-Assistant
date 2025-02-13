002aef1f2369c686e25c0a257066eb45841f00ff
lon5948 <a0988282303@gmail.com>
2025-02-13 10:33:46 +0800

    Add database models and migrations

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.user import User
+
+# You can add additional model imports here as your application grows
+# Example:
+# from app.models.financial_data import FinancialData
+# from app.models.market_analysis import MarketAnalysis
+
+__all__ = ['User']
