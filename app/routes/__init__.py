fe915550a04b8a11b9530f5840d680a9df325509
lon5948 <a0988282303@gmail.com>
2025-02-14 17:16:06 +0800

    fix: remove api blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,5 @@
 from app.routes.auth import auth
 from app.routes.main import main
-from app.routes.api import api
 from app.routes.report import report
 from app.routes.vis import vis as vis_blueprint
 
@@ -8,6 +7,5 @@ from app.routes.vis import vis as vis_blueprint
 def init_app(app):
     app.register_blueprint(auth)
     app.register_blueprint(main)
-    app.register_blueprint(api)
     app.register_blueprint(vis_blueprint)
     app.register_blueprint(report)
